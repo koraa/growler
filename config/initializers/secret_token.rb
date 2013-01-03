f0bd8b8a8344bb4ab2a0c1a51e89e406a3728ac2
Karolin Varner <karo@cupdev.net>
2013-01-03 17:14:15 +0000

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EduCrawl::Application.config.secret_token = 'e3d5a4d510318b7e97e16af74ffa020a31834cea0953593b834e5bdba85d3b480d1a1818d3d4773b7da853b339586120ad2da871c53b1c766b8cfb1cd0a7a1e6'
