65517a1a298aead7f0b86f3f7926b335f85bda14
Karolin Varner <karo@cupdev.net>
2013-01-11 02:18:57 +0100

    Init admin pane

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 EduCrawl::Application.routes.draw do
+  root :to => "adminpane#index"
+  get "adminpane/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
